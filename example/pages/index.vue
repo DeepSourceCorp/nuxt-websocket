853bb304fd62a40e62b9cf4e9b0640fa14aa2438
james-deepsource <james@deepsource.io>
2022-01-28 00:27:20 +0530

    chore: update example

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,11 +1,67 @@
 <template>
   <div>
-    Works!
+    <p>
+      {{ connectionStatus }}
+    </p>
+    <button :disabled="canCloseConnection" @click="openConnection">
+      Connect to WS endpoint
+    </button>
+
+    <p v-if="msgSend && msgReceived">
+      Data received: {{ msgReceived }}
+    </p>
+
+    <form>
+      <label for="messageInput">Message:</label>
+      <input id="messageInput" v-model="msgSend" type="text" name="msg">
+    </form>
+
+    <button @click="sendMessage">
+      Send message
+    </button>
+    <button :disabled="canOpenConnection" @click="closeConnection">
+      Close connection
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      canOpenConnection: false,
+      canCloseConnection: true,
+      connectionStatus: '',
+      msgSend: 'Hello world',
+      msgReceived: ''
+    }
+  },
+  mounted () {
+    this.$socket.$on('socket', data => (this.msgReceived = data))
+    this.connectionStatus = 'Connection established!'
+  },
+  beforeDestroy () {
+    this.$socket.$off('socket')
+  },
+  methods: {
+    openConnection () {
+      this.$socketManager.connect()
+      this.connectionStatus = 'Connection established!'
+      this.canOpenConnection = false
+      this.canCloseConnection = true
+    },
+    closeConnection () {
+      this.$socketManager.ws.close()
+      this.connectionStatus = 'Connection closed!'
+      this.canCloseConnection = false
+      this.canOpenConnection = true
+    },
+    sendMessage () {
+      this.$socketManager.send({ event: 'socket', data: this.msgSend })
+      this.connectionStatus = 'Connection established!'
+      this.canOpenConnection = false
+      this.canCloseConnection = true
+    }
+  }
 }
 </script>
