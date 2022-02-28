b7ee768117a3da99a13fefe51524ab4a659019b7
james-deepsource <james@deepsource.io>
2022-02-28 15:58:01 +0530

    test: update test suite

---
+++ test/module.test.ts
@@ -0,0 +1,28 @@
+import {
+  expectModuleToBeCalledWith,
+  getNuxt,
+  setupTest
+} from '@nuxt/test-utils'
+
+describe('module', () => {
+  setupTest({
+    testDir: __dirname,
+    fixture: '../example'
+  })
+
+  test('should register the plugin', () => {
+    expectModuleToBeCalledWith('addPlugin', {
+      src: 'src/templates/plugin.ts',
+      fileName: 'nuxt-websocket/websocket.client.ts',
+      options: getNuxt().options.websocket
+    })
+  })
+
+  test('should render the template', () => {
+    expectModuleToBeCalledWith('addTemplate', {
+      src: 'src/templates/WebSocketManager.ts',
+      fileName: 'nuxt-websocket/WebSocketManager.ts',
+      options: getNuxt().options.websocket
+    })
+  })
+})
